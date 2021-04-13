8321be643535957731f6759c54d3f15e7928056e
gzsyr <348299953@qq.com>
2021-04-13 14:02:34 +0800

    更新首页页面元素

--- page_object/indexpage/main.py
+++ page_object/indexpage/main.py
@@ -8,14 +8,106 @@ class Main(BasePage):
     """
     首页，各点击入口
     """
-    def goto_newhouse(self):
+
+
+
+    #以下是功能入口
+    def goto_func_entrance_newhouse(self):
         """
-        点击横幅广告
+        点击:功能入口-新房
         :return:
         """
-        with allure.step("点击新房icon"):
-            # self.steps("../../page_object/indexpage/main.yaml")
+        with allure.step("点击功能入口的新房icon"):
             self.steps("../../page_object/indexpage/main.yaml")
         self.tsleep(2)
         return NewHouseList(self._driver)
 
+    def goto_func_entrance_esf(self):
+        """
+        点击:功能入口-二手房
+        :return:
+        """
+        with allure.step("点击功能入口的二手房icon"):
+            self.steps("../../page_object/indexpage/main.yaml")
+        self.tsleep(2)
+        return self
+
+    def goto_func_entrance_zf(self):
+        """
+        点击:功能入口-租房
+        :return:
+        """
+        with allure.step("点击功能入口的租房icon"):
+            self.steps("../../page_object/indexpage/main.yaml")
+        self.tsleep(2)
+        return self
+
+    def goto_func_entrance_spbg(self):
+        """
+        点击:功能入口-商铺办公
+        :return:
+        """
+        with allure.step("点击功能入口的商铺办公icon"):
+            self.steps("../../page_object/indexpage/main.yaml")
+        self.tsleep(2)
+        return self
+
+    def goto_func_entrance_fyfb(self):
+        """
+        点击:功能入口-房源发布
+        :return:
+        """
+        with allure.step("点击功能入口的房源发布icon"):
+            self.steps("../../page_object/indexpage/main.yaml")
+        self.tsleep(2)
+        return self
+
+    def goto_func_entrance_gfgj(self):
+        """
+        点击:功能入口-购房工具
+        :return:
+        """
+        with allure.step("点击功能入口的购房工具icon"):
+            self.steps("../../page_object/indexpage/main.yaml")
+        self.tsleep(2)
+        return self
+
+    def goto_func_entrance_map(self):
+        """
+        点击:功能入口-地图
+        :return:
+        """
+        with allure.step("点击功能入口的地图icon"):
+            self.steps("../../page_object/indexpage/main.yaml")
+        self.tsleep(2)
+        return self
+
+    def goto_func_entrance_school_map(self):
+        """
+        点击:功能入口-学校地图
+        :return:
+        """
+        with allure.step("点击功能入口的学校地图icon"):
+            self.steps("../../page_object/indexpage/main.yaml")
+        self.tsleep(2)
+        return self
+
+    def goto_func_entrance_check_price(self):
+        """
+        点击:功能入口-查房价
+        :return:
+        """
+        with allure.step("点击功能入口的查房价icon"):
+            self.steps("../../page_object/indexpage/main.yaml")
+        self.tsleep(2)
+        return self
+
+    def goto_func_entrance_find_community(self):
+        """
+        点击:功能入口-找小区
+        :return:
+        """
+        with allure.step("点击功能入口的找小区icon"):
+            self.steps("../../page_object/indexpage/main.yaml")
+        self.tsleep(2)
+        return self
