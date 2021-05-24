cd7d7ec311575293de02ccbe02ede39953864e2e
gzsyr <348299953@qq.com>
2021-05-24 13:18:21 +0800

    更新代码

--- test_case/test_case/test_main.py
+++ test_case/test_case/test_main.py
@@ -37,20 +37,11 @@ class TestMain(TestBase):
     def test_click_trade_process(self, cancel_adv):
         cancel_adv(self.app._driver)
         self.shouye.func_entrance_swipe_left("找小区").goto_func_entrance_trade_process().screenshot()
-        # self.shouye.cancel_index_adv().func_entrance_swape_left().goto_func_entrance_trade_process()
 
     @allure.description("点击功能入口第三屏的求租")
     def test_click_find_house(self, cancel_adv):
         cancel_adv(self.app._driver)
         self.shouye.func_entrance_swipe_left("找小区").func_entrance_swipe_left("家居").goto_func_entrance_find_house().screenshot()
-
-    # @allure.description("点击新房列表页面，列表项的第一个楼盘，进入详情页")
-    # def test_goto_newhouse_detail(self):
-    #     self.shouye.goto_newhouse().goto_newhouse_detail().screenshot()
-    #
-    # @allure.description("点击新房详情页面的“查看更多”，进入相册页面")
-    # def test_goto_newhouse_detail(self):
-    #     self.shouye.goto_newhouse().goto_newhouse_detail().goto_photo().screenshot()
 
     @allure.description("点击“购房工具”功能入口，进入购房工具箱页面")
     def test_click_gfgj(self):
