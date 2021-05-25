59bcb72ae1c040006b9642372df12a5294f76368
gzsyr <348299953@qq.com>
2021-05-25 15:59:06 +0800

    添加二手房模块

---
+++ page_object/sellhouse/sellhousedetail.py
@@ -0,0 +1,14 @@
+from page_object.base_page.base_page import BasePage
+
+
+class SellHouseDetail(BasePage):
+    """
+    二手房详情页面
+    """
+    def goto_sell_photo(self):
+        """
+
+        :return:
+        """
+        pass
+
---
+++ page_object/sellhouse/sellhouselist.py
@@ -0,0 +1,14 @@
+from page_object.base_page.base_page import BasePage
+
+
+class SellHouseList(BasePage):
+    """
+    二手房列表页面
+    """
+    def goto_search(self):
+        """
+        点击搜索框
+        :return:
+        """
+        pass
+
