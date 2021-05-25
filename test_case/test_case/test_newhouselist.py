8065acdd1e50a4f7bdaca3fe226a8533e062d229
gzsyr <348299953@qq.com>
2021-05-25 13:42:52 +0800

    更新用例

--- test_case/test_case/test_newhouselist.py
+++ test_case/test_case/test_newhouselist.py
@@ -1,5 +1,6 @@
 import allure
 
+from page_object.newhouse.newhousedetail import NewHouseDetail
 from test_case.base_test.test_base import TestBase
 
 
@@ -7,6 +8,14 @@ class TestNewHouseList(TestBase):
     """
     新房列表页面的所有用例
     """
+
+    @allure.description("点击第一个楼盘")
+    def test_goto_newhouse_detail(self):
+        """
+        点击列表项第一个楼盘，进入详情页面
+        :return:
+        """
+        self.shouye.goto_func_entrance_newhouse().goto_newhouse_detail().screenshot()
 
     @allure.description("点击筛选项：位置->区域")
     def test_click_position_region(self):
@@ -34,7 +43,7 @@ class TestNewHouseList(TestBase):
             screenshot()
 
     @allure.description("点击筛选项：位置->附近->1km")
-    def test_click_position_region_area(self):
+    def test_click_position_region_km(self):
         """
         点击筛选项：位置->区域->建邺区
         :return:
@@ -47,15 +56,16 @@ class TestNewHouseList(TestBase):
             screenshot()
 
     @allure.description("点击筛选项：位置->地铁->1号线->迈皋桥站")
-    def test_click_position_region_area(self):
+    def test_click_position_region_metro(self):
         """
         点击筛选项：位置->地铁->1号线->迈皋桥站
         :return:
         """
-        self.shouye. \
+        a = self.shouye. \
             goto_func_entrance_newhouse(). \
             click_filter_position(). \
             click_filter_position_menu("地铁"). \
             click_filter_position_menu("一号线"). \
-            click_filter_position_menu("迈皋桥站").\
-            screenshot()
+            click_filter_position_menu("迈皋桥站")
+        a.screenshot()
+        a.click_filter_position().screenshot()
