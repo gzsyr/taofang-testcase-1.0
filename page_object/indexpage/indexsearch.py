2965d0da8454f2f2498059d02023c7dfea6f0a16
gzsyr <348299953@qq.com>
2021-05-27 17:42:35 +0800

    更新搜索、新房详情页用例

--- page_object/indexpage/indexsearch.py
+++ page_object/indexpage/indexsearch.py
@@ -16,7 +16,7 @@ class IndexSearch(BasePage):
         """
         self._params["house_name"] = keyword
         with allure.step("输入搜索关键词: " + self._params["house_name"]):
-            self.steps("../../page_object/indexpage/main.yaml", replace=True)
+            self.steps("../../page_object/indexpage/indexsearch.yaml", replace=True)
 
         # 切换输入法，使用键盘的“搜索”键
         self.adbshell('adb shell ime set com.sohu.inputmethod.sogou/.SogouIME')
@@ -30,7 +30,7 @@ class IndexSearch(BasePage):
     def select_search_result(self, housename = None):
         # self._params["house_name"] = housename
         with allure.step("点击搜索结果楼盘页面"):
-            self.steps("../../page_object/indexpage/main.yaml", replace=True)
+            self.steps("../../page_object/indexpage/indexsearch.yaml", replace=True)
 
         return NewHouseDetail(self._driver)
 
--- test_case/test_case/test_newhousedetail.py
+++ test_case/test_case/test_newhousedetail.py
@@ -4,12 +4,13 @@ from test_case.base_test.test_base import TestBase
 
 class TestNewHouseDetail(TestBase):
     """
-    新房详情页用例
+    新房详情页用例，通过搜索的方式进入新房详情页
     """
-    _house = "万科翡翠天域"
+    _house = "华侨城翡翠天域"
 
     def test_goto_housedetail(self):
         """
         进入新房详情页
         :return:
         """
+        self.shouye.goto_search().action_search(self._house).select_search_result().screenshot()
