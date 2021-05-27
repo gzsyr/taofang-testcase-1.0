53f67f3411c7f53fa62977b3cc943d803527f4e9
gzsyr <348299953@qq.com>
2021-05-27 16:59:50 +0800

    增加首页搜索

---
+++ page_object/indexpage/indexsearch.py
@@ -0,0 +1,14 @@
+from page_object.base_page.base_page import BasePage
+
+
+class IndexSearch(BasePage):
+    """
+    首页，点击搜索框后的页面
+    """
+    def goto_search(self):
+        """
+
+        :return:
+        """
+        pass
+
