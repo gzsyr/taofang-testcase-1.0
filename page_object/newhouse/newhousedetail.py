ed6a65bb96938a2aa4d5d2589d6339a1c73eb6cd
copment <1617874115@qq.com>
2021-05-28 14:13:22 +0800

    更新新房详情元素

--- page_object/newhouse/newhousedetail.py
+++ page_object/newhouse/newhousedetail.py
@@ -50,3 +50,366 @@ class NewHouseDetail(BasePage):
         self.tsleep(2)
 
         return self
+
+    def click_newhouse_detail_calculator(self):
+        """
+        新房详情页点击计算器
+        :return:
+        """
+        with allure.step("新房详情页点击计算器"):
+            self.steps("../../page_object/newhouse/newhousedetail.yaml")
+        self.tsleep(2)
+
+        return self
+
+    def click_newhouse_detail_arrow(self):
+        """
+        新房详情页点击地址旁边的箭头
+        :return:
+        """
+        with allure.step("新房详情页点击地址旁边的箭头"):
+            self.steps("../../page_object/newhouse/newhousedetail.yaml")
+        self.tsleep(2)
+
+        return self
+
+    def click_newhouse_detail_questions(self):
+        """
+        新房详情页点击悬浮提问
+        :return:
+        """
+        with allure.step("新房详情页点击悬浮提问"):
+            self.steps("../../page_object/newhouse/newhousedetail.yaml")
+        self.tsleep(2)
+
+        return self
+
+    def click_newhouse_detail_400tel(self):
+        """
+        新房详情页点击400电话按钮
+        :return:
+        """
+        with allure.step("新房详情页点击400电话按钮"):
+            self.steps("../../page_object/newhouse/newhousedetail.yaml")
+        self.tsleep(2)
+
+        return self
+
+    def click_newhouse_detail_kptz(self):
+        """
+        新房详情页点击开盘通知我
+        :return:
+        """
+        with allure.step("新房详情页点击开盘通知我"):
+            self.steps("../../page_object/newhouse/newhousedetail.yaml")
+        self.tsleep(2)
+
+        return self
+
+    def click_newhouse_detail_jjtz(self):
+        """
+        新房详情页点击降价通知我
+        :return:
+        """
+        with allure.step("新房详情页点击降价通知我"):
+            self.steps("../../page_object/newhouse/newhousedetail.yaml")
+        self.tsleep(2)
+
+        return self
+
+    def click_newhouse_detail_key_group(self):
+        """
+        新房详情页点击一键加群
+        :return:
+        """
+        with allure.step("新房详情页点击一键加群"):
+            self.steps("../../page_object/newhouse/newhousedetail.yaml")
+        self.tsleep(2)
+
+        return self
+
+    def click_newhouse_detail_cope_wechat(self):
+        """
+        新房详情页点击复制微信号
+        :return:
+        """
+        with allure.step("新房详情页点击复制微信号"):
+            self.steps("../../page_object/newhouse/newhousedetail.yaml")
+        self.tsleep(2)
+
+        return self
+
+    def click_newhouse_detail_signup(self):
+        """
+        新房详情页点击我要预约
+        :return:
+        """
+        with allure.step("新房详情页点击我要预约"):
+            self.steps("../../page_object/newhouse/newhousedetail.yaml")
+        self.tsleep(2)
+
+        return self
+
+    def click_newhouse_detail_check_detail(self):
+        """
+        新房详情页点击看房团查看详情
+        :return:
+        """
+        with allure.step("新房详情页点击看房团查看详情"):
+            self.steps("../../page_object/newhouse/newhousedetail.yaml")
+        self.tsleep(2)
+
+        return self
+
+    def click_newhouse_detail_developmen(self):
+        """
+        新房详情页点击最新动态
+        :return:
+        """
+        with allure.step("新房详情页点击最新动态"):
+            self.steps("../../page_object/newhouse/newhousedetail.yaml")
+        self.tsleep(2)
+
+        return self
+
+    def click_newhouse_detail_hxjiexi(self):
+        """
+        新房详情页点击户型解析
+        :return:
+        """
+        with allure.step("新房详情页点击户型解析"):
+            self.steps("../../page_object/newhouse/newhousedetail.yaml")
+        self.tsleep(2)
+
+        return self
+
+    def click_newhouse_detail_house_detail(self):
+        """
+        新房详情页点击楼盘详情
+        :return:
+        """
+        with allure.step("新房详情页点击楼盘详情"):
+            self.steps("../../page_object/newhouse/newhousedetail.yaml")
+        self.tsleep(2)
+
+        return self
+
+    def click_newhouse_detail_house_comment(self):
+        """
+        新房详情页点击楼盘点评
+        :return:
+        """
+        with allure.step("新房详情页点击楼盘点评"):
+            self.steps("../../page_object/newhouse/newhousedetail.yaml")
+        self.tsleep(2)
+
+        return self
+
+    def click_newhouse_detail_yifangyijia(self):
+        """
+        新房详情页点击一房一价
+        :return:
+        """
+        with allure.step("新房详情页点击一房一价"):
+            self.steps("../../page_object/newhouse/newhousedetail.yaml")
+        self.tsleep(2)
+
+        return self
+
+    def click_newhouse_detail_dynamic_arrow(self):
+        """
+        新房详情页点击楼盘动态右侧箭头按钮
+        :return:
+        """
+        with allure.step("新房详情页点击楼盘动态右侧箭头按钮"):
+            self.steps("../../page_object/newhouse/newhousedetail.yaml")
+        self.tsleep(2)
+
+        return self
+
+    def click_newhouse_detail_more_ico(self):
+        """
+        新房详情页点击楼盘动态内容下拉箭头
+        :return:
+        """
+        with allure.step("新房详情页点击楼盘动态内容下拉箭头"):
+            self.steps("../../page_object/newhouse/newhousedetail.yaml")
+        self.tsleep(2)
+
+        return self
+
+    def click_newhouse_detail_desc_short(self):
+        """
+        新房详情页点击楼盘动态内容
+        :return:
+        """
+        with allure.step("新房详情页点击楼盘动态内容"):
+            self.steps("../../page_object/newhouse/newhousedetail.yaml")
+        self.tsleep(2)
+
+        return self
+
+    def click_newhouse_detail_xxnr_more_ico(self):
+        """
+        新房详情页点击详细信息内容下拉箭头
+        :return:
+        """
+        with allure.step("新房详情页点击详细信息内容下拉箭头"):
+            self.steps("../../page_object/newhouse/newhousedetail.yaml")
+        self.tsleep(2)
+
+        return self
+
+    def click_newhouse_detail_error_correction(self):
+        """
+        新房详情页点击详细信息纠错
+        :return:
+        """
+        with allure.step("新房详情页点击详细信息纠错"):
+            self.steps("../../page_object/newhouse/newhousedetail.yaml")
+        self.tsleep(2)
+
+        return self
+
+    def click_newhouse_detail_wzzb_address(self):
+        """
+        新房详情页点击位置及周边右侧箭头
+        :return:
+        """
+        with allure.step("新房详情页点击位置及周边右侧箭头"):
+            self.steps("../../page_object/newhouse/newhousedetail.yaml")
+        self.tsleep(2)
+
+        return self
+
+    def click_newhouse_detail_wzzb_more_ico(self):
+        """
+        新房详情页点击位置及周边内容下拉箭头
+        :return:
+        """
+        with allure.step("新房详情页点击位置及周边内容下拉箭头"):
+            self.steps("../../page_object/newhouse/newhousedetail.yaml")
+        self.tsleep(2)
+
+        return self
+
+    def click_newhouse_detail_tv_plan(self):
+        """
+        新房详情页点击位置及周边“咨询楼盘配套及区域规划”
+        :return:
+        """
+        with allure.step("新房详情页点击位置及周边“咨询楼盘配套及区域规划”"):
+            self.steps("../../page_object/newhouse/newhousedetail.yaml")
+        self.tsleep(2)
+
+        return self
+
+    def click_newhouse_detail_map_image(self):
+        """
+        新房详情页点击位置及周边地图
+        :return:
+        """
+        with allure.step("新房详情页点击位置及周边地图"):
+            self.steps("../../page_object/newhouse/newhousedetail.yaml")
+        self.tsleep(2)
+
+        return self
+
+    def click_newhouse_detail_imageview(self):
+        """
+        新房详情页点击楼盘点评右侧箭头
+        :return:
+        """
+        with allure.step("新房详情页点击楼盘点评右侧箭头"):
+            self.steps("../../page_object/newhouse/newhousedetail.yaml")
+        self.tsleep(2)
+
+        return self
+
+    def click_newhouse_detail_bt_comment(self):
+        """
+        新房详情页点击楼盘点评“点评赚淘房币，立即点评”
+        :return:
+        """
+        with allure.step("新房详情页点击楼盘点评“点评赚淘房币，立即点评”"):
+            self.steps("../../page_object/newhouse/newhousedetail.yaml")
+        self.tsleep(2)
+
+        return self
+
+    def click_newhouse_detail_fbs_right_arrow(self):
+        """
+        新房详情页点击房博士解读右侧更多按钮
+        :return:
+        """
+        with allure.step("新房详情页点击房博士解读右侧更多按钮"):
+            self.steps("../../page_object/newhouse/newhousedetail.yaml")
+        self.tsleep(2)
+
+        return self
+
+    def click_newhouse_detail_bt_ask(self):
+        """
+        新房详情页点击房博士解读“我要提问”
+        :return:
+        """
+        with allure.step("新房详情页点击房博士解读“我要提问”"):
+            self.steps("../../page_object/newhouse/newhousedetail.yaml")
+        self.tsleep(2)
+
+        return self
+
+    def click_newhouse_detail_tv_title_ask(self):
+        """
+        新房详情页点击房博士解读内容
+        :return:
+        """
+        with allure.step("新房详情页点击房博士解读内容"):
+            self.steps("../../page_object/newhouse/newhousedetail.yaml")
+        self.tsleep(2)
+
+        return self
+
+    def click_newhouse_detail_tfh_discount(self):
+        """
+        新房详情页点击申请优惠
+        :return:
+        """
+        with allure.step("新房详情页点击申请优惠"):
+            self.steps("../../page_object/newhouse/newhousedetail.yaml")
+        self.tsleep(2)
+
+        return self
+
+    def click_newhouse_detail_look_house(self):
+        """
+        新房详情页点击我要看房
+        :return:
+        """
+        with allure.step("新房详情页点击我要看房"):
+            self.steps("../../page_object/newhouse/newhousedetail.yaml")
+        self.tsleep(2)
+
+        return self
+
+    def click_newhouse_detail_online_chat_tfh(self):
+        """
+        新房详情页点击在线咨询
+        :return:
+        """
+        with allure.step("新房详情页点击在线咨询"):
+            self.steps("../../page_object/newhouse/newhousedetail.yaml")
+        self.tsleep(2)
+
+        return self
+
+    def click_newhouse_detail_call_tfh(self):
+        """
+        新房详情页点击拨打电话
+        :return:
+        """
+        with allure.step("新房详情页点击拨打电话"):
+            self.steps("../../page_object/newhouse/newhousedetail.yaml")
+        self.tsleep(2)
+
+        return self
