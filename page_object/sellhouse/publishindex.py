19e84cf519fe57ff4ebf9729ad55824995c628fb
Y-danzi <2801245394@qq.com>
2021-06-01 17:00:54 +0800

    增加发布房源引导页点击函数

--- page_object/sellhouse/publishindex.py
+++ page_object/sellhouse/publishindex.py
@@ -1,3 +1,5 @@
+import allure
+
 from page_object.base_page.base_page import BasePage
 
 
@@ -5,3 +7,64 @@ class PublishIndex(BasePage):
     """
     发布房源  引导页面
     """
+
+    def click_publishindex_m_publish(self):
+        """
+        点击发布房源引导页的发布房源按钮
+        :return:
+        """
+        with allure.step("点击发布房源引导页的发布房源按钮"):
+            self.steps("../../page_object/sellhouse/publishindex.yaml")
+        self.tsleep(2)
+        return self
+
+    def click_publishindex_m_help(self):
+        """
+        点击右上角帮助按钮
+        :return:
+        """
+        with allure.step("点击发布房源引导页的右上角帮助按钮按钮"):
+            self.steps("../../page_object/sellhouse/publishindex.yaml")
+        self.tsleep(2)
+        return self
+
+    def click_publishindex_m_service(self):
+        """
+        点击右上角服务按钮
+        :return:
+        """
+        with allure.step("点击发布房源引导页的服务按钮"):
+            self.steps("../../page_object/sellhouse/publishindex.yaml")
+        self.tsleep(2)
+        return self
+
+    def click_publishindex_m_pinggu_img(self):
+        """
+        点击工具评估房价
+        :return:
+        """
+        with allure.step("点击发布房源引导页的评估房价按钮"):
+            self.steps("../../page_object/sellhouse/publishindex.yaml")
+        self.tsleep(2)
+        return self
+
+    def click_publishindex_m_xuequ_img(self):
+        """
+        点击学校地图按钮
+        :return:
+        """
+        with allure.step("点击发布房源引导页的学校地图按钮"):
+            self.steps("../../page_object/sellhouse/publishindex.yaml")
+        self.tsleep(2)
+        return self
+
+    def click_publishindex_m_jisuanqi_img(self):
+        """
+        点击计算器按钮
+        :return:
+        """
+        with allure.step("点击发布房源引导页的计算器按钮"):
+            self.steps("../../page_object/sellhouse/publishindex.yaml")
+        self.tsleep(2)
+        return self
+
