5531419e22adf59d0dbbdb1be26f357f4f21ab8e
zhangzhihui19950725 <1872135605@qq.com>
2021-06-07 13:29:41 +0800

    更新用例

--- test_case/test_case/test_sellhouselist.py
+++ test_case/test_case/test_sellhouselist.py
@@ -213,7 +213,7 @@ class TestSellHouseList(TestBase):
         二手房-筛选：总价-200-250万
         """
         self.shouye.\
-            goto_func_entrance_esf().click_empty().\
+            goto_func_entrance_esf().\
             click_filter_price().\
             click_filter_position_menu("200-250万").\
             screenshot()
@@ -224,7 +224,7 @@ class TestSellHouseList(TestBase):
         二手房-筛选：总价-自定义，150-280万，确定
         """
         step = self.shouye.goto_func_entrance_esf().\
-            click_empty().click_filter_price().\
+            click_filter_price().\
             click_filter_lowest_price("150").\
             click_filter_highest_price("280")
         step.screenshot()
@@ -249,17 +249,19 @@ class TestSellHouseList(TestBase):
         """
         二手房-筛选：更多-个人、住宅、80-100m、简装、1995年后、2-5层、南、无个税、VR看房、产权房，确定
         """
-        step1 = self.shouye.goto_func_entrance_esf().click_empty().click_filter_more().\
+        step1 = self.shouye.goto_func_entrance_esf().click_filter_more().\
             click_filter_position_menu("个人").click_filter_position_menu("住宅").\
             click_filter_position_menu("80-100㎡").click_filter_position_menu("简装")
         step1.screenshot()
-        step2 = step1.swipe_to_buttom("使用权房")
+        step2 = step1.swipe_to_buttom("朝向")
         step2.screenshot()
         step3 = step2.click_filter_position_menu("1995年后").click_filter_position_menu("2-5层"). \
-            click_filter_position_menu("南").click_filter_position_menu("无个税"). \
-            click_filter_position_menu("VR看房").click_filter_position_menu("产权房")
+            click_filter_position_menu("南")
         step3.screenshot()
-        step3.click_filter_confirm().screenshot()
+        step4 = step3.swipe_to_buttom("使用权房").click_filter_position_menu("无个税"). \
+            click_filter_position_menu("VR看房").click_filter_position_menu("产权房")
+        step4.screenshot()
+        step4.click_filter_confirm().screenshot()
 
     @allure.description("点击二手房-筛选：位置-地铁-1号线-迈皋桥站")
     def test_click_screening_location_station(self):
