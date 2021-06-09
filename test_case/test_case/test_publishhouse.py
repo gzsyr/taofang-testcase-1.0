f03511be8fff1341734036566f22340737ac3874
Y-danzi <2801245394@qq.com>
2021-06-09 11:29:14 +0800

    增加出售房源测试用例

--- test_case/test_case/test_publishhouse.py
+++ test_case/test_case/test_publishhouse.py
@@ -8,6 +8,9 @@ class TestPublishHouse(TestBase):
     发布房源相关测试用例
     """
     def goto_publish_sell(self):
+        """
+        首页功能入口竟然我要卖房页面
+        """
         return self.shouye.func_entrance_swipe_left("找小区").goto_func_entrance_publish_sell()
 
     @allure.description("点击引导页的发布房源按钮")
@@ -67,7 +70,7 @@ class TestPublishHouse(TestBase):
         self.goto_publish_sell().click_publishindex_m_publish().click_publishhouse_reset().screenshot()
 
     @allure.description("选择物业类型-住宅")
-    def test_publish_s1(self):
+    def test_publish_type(self):
         """
         选择物业类型-住宅
         :return:
@@ -76,7 +79,7 @@ class TestPublishHouse(TestBase):
             click_publishhouse_type().select_pubilshhouse_type(house_item="物业类型", house_type="住宅").screenshot()
 
     @allure.description("点击小区名称-搜索-万科红郡住宅")
-    def test_publish_s2(self):
+    def test_publish_name(self):
         """
         点击小区名称-搜索-万科红郡住宅
         :return:
@@ -86,7 +89,7 @@ class TestPublishHouse(TestBase):
             select_publishhouse_name_result().screenshot()
 
     @allure.description("输入楼栋：5栋")
-    def test_publish_s3(self):
+    def test_publish_building(self):
         """
         输入楼栋
         :return:
@@ -94,7 +97,7 @@ class TestPublishHouse(TestBase):
         self.goto_publish_sell().click_publishindex_m_publish().send_publishhouse_dong("5").screenshot()
 
     @allure.description("输入单元：1单元")
-    def test_publish_s4(self):
+    def test_publish_unit(self):
         """
         输入单元
         :return:
@@ -102,7 +105,7 @@ class TestPublishHouse(TestBase):
         self.goto_publish_sell().click_publishindex_m_publish().send_publishhouse_danyuan("5").screenshot()
 
     @allure.description("输入室：1006室")
-    def test_publish_s5(self):
+    def test_publish_room(self):
         """
         输入室
         :return:
@@ -110,7 +113,7 @@ class TestPublishHouse(TestBase):
         self.goto_publish_sell().click_publishindex_m_publish().send_publishhouse_shi("1006").screenshot()
 
     @allure.description("输入售价：400万元")
-    def test_publish_s6(self):
+    def test_publish_price(self):
         """
         输入期望售价
         :return:
@@ -118,7 +121,7 @@ class TestPublishHouse(TestBase):
         self.goto_publish_sell().click_publishindex_m_publish().send_publishhouse_price("400").screenshot()
 
     @allure.description("输入面积：116平")
-    def test_publish_s7(self):
+    def test_publish_area(self):
         """
         输入面积
         :return:
@@ -126,7 +129,7 @@ class TestPublishHouse(TestBase):
         self.goto_publish_sell().click_publishindex_m_publish().send_publishhouse_area("116").screenshot()
 
     @allure.description("房屋权属-产权房")
-    def test_publish_s8(self):
+    def test_publish_property(self):
         """
         选择房屋权属
         :return:
@@ -135,7 +138,7 @@ class TestPublishHouse(TestBase):
             select_pubilshhouse_type(house_item="房屋权属", house_type="产权房").screenshot()
 
     @allure.description("输入称呼：李女士")
-    def test_publish_s9(self):
+    def test_publish_seller(self):
         """
         输入出售人名称
         :return:
@@ -143,9 +146,22 @@ class TestPublishHouse(TestBase):
         self.goto_publish_sell().click_publishindex_m_publish().send_publishhouse_seller("李女士").screenshot()
 
     @allure.description("点击出售房源下一步，进入更多资料页")
-    def test_publish_s10(self):
+    def test_publish_more(self):
         """
         点击出售房源下一步，进入更多资料页
         :return:
         """
-        self.goto_publish_sell().click_publishindex_m_publish().goto_next_all().screenshot()
+        step1 = self.goto_publish_sell().click_publishindex_m_publish().goto_next_all()
+        step1.screenshot()
+        step2 = step1.click_publishhouse_next()
+        step2.screenshot()
+
+    @allure.description("更多资料页选择户型")
+    def test_publish_layout(self):
+        """
+        更多资料页选择户型
+        :return:
+        """
+        self.goto_publish_sell().click_publishindex_m_publish().goto_next_all().click_publishhouse_next().\
+            click_publishhouse_room().click_publishhouse_confirm().screenshot()
+
