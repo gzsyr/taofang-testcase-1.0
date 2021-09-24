bcf30bc320afd991462e4b3e3517564eeb5ebdb8
Jennifer <zhusuyun@vip.house365.com>
2021-09-24 13:49:09 +0800

    更新所有用例

--- test_case/test_case/execute_case.py
+++ test_case/test_case/execute_case.py
@@ -13,35 +13,34 @@ if __name__ == "__main__":
                  # "--show-capture=all",
                  "--alluredir", "./../../TestResult",
                  # "--html=./../testreport.html", "--self-contained-html",
-
-                 "./test_businesslist.py",
+                 "./test_businesslist.py",  # 商铺办公列表页面
                  "./test_buyinghouse.py",
-                 "./test_buyingtools.py",
-                 "./test_checkhouseprice.py",
-                 "./test_communitydetail.py",
-                 "./test_communitylist.py",
-                 "./test_housedoctor.py",
-                 "./test_housetour.py",
+                 "./test_buyingtools.py",  # 购房工具页面
+                 "./test_checkhouseprice.py",  # 查房价页面
+                 "./test_communitydetail.py",  # 找小区详情页面
+                 "./test_communitylist.py",  # 找小区列表页面
+                 "./test_housedoctor.py",  # 房博士页面
+                 "./test_housetour.py",  # 看房团页面
                  "./test_main.py",
-                 "./test_mapfindhouse.py",
-                 "./test_minepage.py",
+                 "./test_mapfindhouse.py",  # 地图找房页面
+                 "./test_minepage.py",  # 我的页面
                  "./test_newhousedetail.py",
                  "./test_newhouselist.py",
-                 "./test_newslist.py",
+                 "./test_newslist.py",  # 资讯列表
                  "./test_publishhouse.py",
-                 "./test_publishrental.py",
-                 "./test_rentbrandapartmentdetail.py",
-                 "./test_rentbrandapartmentlist.py",
-                 "./test_rentfindhouse.py",
-                 "./test_renthouse.py",
-                 "./test_renthousedetail.py",
-                 "./test_renthouselist.py",
-                 "./test_rentlookroommatelist.py",
-                 "./test_rentlookroommaterelease.py",
-                 "./test_rentofficebuildingdetail.py",
-                 "./test_rentofficebuildinglist.py",
-                 "./test_rentsingleapartmentdetail.py",
-                 "./test_rentsingleapartmentlist.py",
+                 "./test_publishrental.py",  # 租房-发布出租页面
+                 "./test_rentbrandapartmentdetail.py",  # 租房-品牌公寓详情页
+                 "./test_rentbrandapartmentlist.py",  # 租房-品牌公寓列表
+                 "./test_rentfindhouse.py",  # 租房-我要求租
+                 "./test_renthouse.py",  # 租房-租房首页
+                 "./test_renthousedetail.py",  # 租房-租房房源详情页
+                 "./test_renthouselist.py",  # 租房-租房房源列表页
+                 "./test_rentlookroommatelist.py",  # 租房-找室友列表
+                 "./test_rentlookroommaterelease.py",  # 租房-找室友发布
+                 "./test_rentofficebuildingdetail.py",  # 租房-写字楼详情页
+                 "./test_rentofficebuildinglist.py",  # 租房-写字楼列表
+                 "./test_rentsingleapartmentdetail.py",  # 租房-独栋公寓详情页
+                 "./test_rentsingleapartmentlist.py",  # 租房-独栋公寓列表
                  "./test_search.py",
                  "./test_sellhousedetail.py",
                  "./test_sellhouselist.py",
